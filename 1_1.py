7782b64f875befac0ed988b014ac503a69d5919c
Chris Bolas <tanlaan33@gmail.com>
2023-12-02 11:39:09 -0800

    Add left and right search for character. Successful test

--- 1_1.py
+++ 1_1.py
@@ -1,13 +1,26 @@
 import sys
 
 def main():
+    cal = 0
+    left = 0
+    right = 0
+
     try:
         while(True):
             line = input()
-            print(line)
-            
+            for char in line:
+                if char.isdigit():
+                    left = int(char)
+                    break
+            for char in line[::-1]:
+                if char.isdigit():
+                    right = int(char)
+                    break
+            temp = (left * 10) + right
+            print(temp)
+            cal += temp
     except EOFError:
-        print("Merry XMAS")
+        print(f"Your calibration value is : {cal}")
 
 if __name__ == "__main__":
     main()
